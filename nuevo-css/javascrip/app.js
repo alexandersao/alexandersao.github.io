80763f300a2e5521a1a663222502e686fc4f4482
alexandersao <101808409+alexandersao@users.noreply.github.com>
2022-03-18 13:09:00 -0300

    Creando mi primer sitio web

---
+++ nuevo-css/javascrip/app.js
@@ -0,0 +1,15 @@
+gsap.from('.vi',{
+    duration:3,
+    x:300,
+    // delay:1,
+});
+gsap.to('.so',{
+    duration:3,
+    x:500,
+    // delay:1,
+});
+gsap.from('.im',{
+    duration:3,
+    x:100,
+    // delay:1,
+});
